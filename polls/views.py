90089f74acb67434eec1012cbf7499f88c348d38
rahul chauhan <rah.chauhan1@gmail.com>
2020-11-30 15:04:49 +0530

    model updated

--- polls/views.py
+++ polls/views.py
@@ -6,3 +6,11 @@ from django.http import HttpResponse
 def index(request):
   return HttpResponse("Hello, This is a polling app.")
 
+def detail(request, question_id):
+  return HttpResponse('You are looking at question %s.' %question_id)
+
+def results(request, question_id):
+  return HttpResponse('You are looking at results of question {id}'.format(id=question_id))
+
+def vote(request, question_id):
+  return HttpResponse('You are voting on question {id}'.format(id=question_id))
